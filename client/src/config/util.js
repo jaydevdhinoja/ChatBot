09f9ec9e7873160c9ee8b6ea641eac2e03407a89
Jaydev Dhinoja <jaydev@gmail.com>
2022-11-08 20:41:41 +0000

    updated env variable

--- client/src/config/util.js
+++ client/src/config/util.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.APIURL || "http://localhost:3001",
+  baseURL: process.env.REACT_APP_APIURL || "http://localhost:3001",
 });
